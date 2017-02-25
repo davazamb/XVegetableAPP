b007dbeb106510623cd4276e49b3143c8213764c
David Zambrano <davazamb10@outlook.com>
2017-02-25 11:17:44 -0400

    Included region in vegetableitemviewmodel

---
+++ XVegetableAPP/XVegetableAPP/ViewModels/VegetableItemViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XVegetableAPP.Models;
+
+namespace XVegetableAPP.ViewModels
+{
+    public class VegetableItemViewModel : Vegetable
+    {
+        #region Attributes
+
+        #endregion
+
+        #region Commands
+
+        #endregion
+
+        #region Constructors
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+
+        #region Properties
+
+        #endregion      
+
+
+
+    }
+}
