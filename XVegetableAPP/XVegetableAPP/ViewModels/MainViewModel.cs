e8d72b0cd3ac72e34b85637a3290018a52548335
David Zambrano <davazamb10@outlook.com>
2017-02-25 11:43:39 -0400

    Changed the mainviewmodel and listview in vegetablepage

--- XVegetableAPP/XVegetableAPP/ViewModels/MainViewModel.cs
+++ XVegetableAPP/XVegetableAPP/ViewModels/MainViewModel.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using XVegetableAPP.Models;
+using XVegetableAPP.Services;
 
 namespace XVegetableAPP.ViewModels
 {
     public class MainViewModel
     {
         #region Attributes
+        private ApiService apiService;
+
 
         #endregion
 
@@ -17,14 +22,40 @@ namespace XVegetableAPP.ViewModels
         #endregion
 
         #region Constructors
+        public MainViewModel()
+        {
+            apiService = new ApiService();
+            Vegetables = new ObservableCollection<VegetableItemViewModel>();
+            LoadVegetables();
+        }  
 
         #endregion
 
         #region Methods
+        private async void LoadVegetables()
+        {
+            var vegetables = await apiService.Get<Vegetable>("http://vegetableapi.azurewebsites.net", "/api", "/Vegetables");
+            ReloadVegetables(vegetables);
+        }
+
+        private void ReloadVegetables(List<Vegetable> vegetables)
+        {
+            Vegetables.Clear();
+            foreach (var vegetable in vegetables)
+            {
+                Vegetables.Add(new VegetableItemViewModel
+                {
+                    Description = vegetable.Description,
+                    VegetableId = vegetable.VegetableId,
+                    Price = vegetable.Price,
+                });
+            }
+        }
 
         #endregion
 
         #region Properties
+        public ObservableCollection<VegetableItemViewModel> Vegetables { get; set; }
 
         #endregion
     }
