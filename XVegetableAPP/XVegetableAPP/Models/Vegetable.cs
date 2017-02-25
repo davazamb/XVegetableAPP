3345b9febddabf75c1af0bcdd509a3a524ad7ef6
David Zambrano <davazamb10@outlook.com>
2017-02-25 11:04:38 -0400

    create apiservice and model vegetable

---
+++ XVegetableAPP/XVegetableAPP/Models/Vegetable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XVegetableAPP.Models
+{
+    public class Vegetable
+    {
+        public int VegetableId { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public override int GetHashCode()
+        {
+            return VegetableId;
+        }
+
+    }
+}
---
+++ XVegetableAPP/XVegetableAPP/Services/ApiService.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XVegetableAPP.Services
+{
+    public class ApiService
+    {
+        public async Task<List<T>> Get<T>(string urlBase, string servicePrefix, string controller)
+        {
+            try
+            {
+                var client = new HttpClient();
+                client.BaseAddress = new Uri(urlBase);
+                var url = string.Format("{0}{1}", servicePrefix, controller);
+                var response = await client.GetAsync(url);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    return null;
+                }
+
+                var result = await response.Content.ReadAsStringAsync();
+                var list = JsonConvert.DeserializeObject<List<T>>(result);
+                return list;
+            }
+            catch
+            {
+                return null;
+            }
+        } 
+    }
+}
